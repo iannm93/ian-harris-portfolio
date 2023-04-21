6ba2443623b14cc650a8076b4da4366c7ca7d4e1
Ian Harris <ianmharris93@gmail.com>
2023-04-21 13:30:45 -0700

    ch

--- js/scrollElements.js
+++ js/scrollElements.js
@@ -1,19 +1,19 @@
-let numWork = 1
-let reveal = () =>{
-    let revealSelector = document.querySelectorAll('.revealSelector') 
+// let numWork = 1
+// let reveal = () =>{
+//     let revealSelector = document.querySelectorAll('.revealSelector') 
     
-    for( let i =0; i< revealSelector.length; i++){
+//     for( let i =0; i< revealSelector.length; i++){
         
-        let winHeight = window.innerHeight
-        let revealTop = revealSelector[i].getBoundingClientRect().top
-        let revealPoint = 150;
+//         let winHeight = window.innerHeight
+//         let revealTop = revealSelector[i].getBoundingClientRect().top
+//         let revealPoint = 150;
         
-        if (revealTop < winHeight - revealPoint){
-            console.log(numWork)
-            revealSelector[i].classList.add('active')
-        } 
-        console.log(revealSelector[i].textContent)
-    }
-}
+//         if (revealTop < winHeight - revealPoint){
+//             console.log(numWork)
+//             revealSelector[i].classList.add('active')
+//         } 
+//         console.log(revealSelector[i].textContent)
+//     }
+// }
 
-window.addEventListener("scroll", reveal)
+// window.addEventListener("scroll", reveal)
