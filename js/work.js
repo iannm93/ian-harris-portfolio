2ee65405caaaae2d47e3fd484f388a081ef4deff
Ian Harris <ianmharris93@gmail.com>
2023-05-16 15:36:54 -0700

    hamburger

--- js/work.js
+++ js/work.js
@@ -3,6 +3,8 @@
  let bar1 = document.querySelector(".bar1")
  let bar2 = document.querySelector(".bar2")
  let bar3= document.querySelector(".bar3")
+let responsiveContents = document.querySelector(".responsive-contents")
+
  console.log("test")
  let switched = false
 
@@ -16,6 +18,9 @@ let hamburger = () =>{
         bar3.style.transform= "translateY(-5px) rotate(-45deg)"
         
         bar2.style.opacity = 0;
+        responsiveContents.style.opacity = 1;
+        responsiveContents.style.transition = ".4s all"
+
         switched = true;
     }else{
         bar1.style.transform= "rotate(0deg)"
@@ -27,10 +32,16 @@ let hamburger = () =>{
         bar2.style.opacity = 1;
         bar2.style.transition =".4s all" 
 
+        responsiveContents.style.opacity = 0;
+        responsiveContents.style.transition = ".4s all"
+
         switched = false;
     }
 
 
 
-}
+}   
+
+
+
  bar.addEventListener("click", hamburger)
